f151f5732d32e0560de9a1e1ca97b5a80df169f1
Federico Gentili <federicogentili05@gmail.com>
2025-06-08 09:32:23 +0200

    added video in the authentication layout

---
+++ resources/js/layouts/auth/AuthSplitBackgroundVideo.vue
@@ -0,0 +1,12 @@
+<template>
+  <video
+    class="absolute inset-0 h-full w-full object-cover bg-black"
+    autoplay
+    muted
+    loop
+    playsinline
+    preload="auto"
+  >
+    <source src="/videos/auth-background.mp4" type="video/mp4" />
+  </video>
+</template>
--- resources/js/layouts/auth/AuthSplitLayout.vue
+++ resources/js/layouts/auth/AuthSplitLayout.vue
@@ -1,10 +1,38 @@
 <script setup lang="ts">
 import AppLogoIcon from '@/components/AppLogoIcon.vue';
 import { Link, usePage } from '@inertiajs/vue3';
+import { defineAsyncComponent, ref, reactive, toRefs, onMounted, onUnmounted } from 'vue';
 
 const page = usePage();
-const name = page.props.name;
-const quote = page.props.quote;
+const reactiveProps = reactive(page.props);
+const { name, quote } = toRefs(reactiveProps);
+
+
+const LazyVideo = defineAsyncComponent({
+    loader: () => import('./AuthSplitBackgroundVideo.vue'),
+})
+
+const showVideo = ref(false);
+
+onMounted(() => {
+    const mediaQuery = window.matchMedia('(min-width: 1024px)');
+
+    if (mediaQuery.matches) {
+        showVideo.value = true;
+    }
+
+    const handler = (event: MediaQueryListEvent) => {
+        if (event.matches) {
+            showVideo.value = true;
+        }
+    };
+
+    mediaQuery.addEventListener('change', handler);
+
+    onUnmounted(() => {
+        mediaQuery.removeEventListener('change', handler);
+    });
+});
 
 defineProps<{
     title?: string;
@@ -15,7 +43,8 @@ defineProps<{
 <template>
     <div class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div class="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-            <div class="absolute inset-0 bg-zinc-900" />
+            <LazyVideo v-if="showVideo" />
+            <div class="absolute inset-0 bg-zinc-900/60" />
             <Link :href="route('home')" class="relative z-20 flex items-center text-lg font-medium w-fit">
                 <AppLogoIcon class="mr-2 size-8 fill-current text-white" />
                 {{ name }}
@@ -28,7 +57,7 @@ defineProps<{
             </div>
         </div>
         <div class="lg:p-8">
-            <div class="mx-auto flex w-full flex-col justify-center space-y-6 p-6 sm:w-[350px]">
+            <div class="mx-auto flex w-full flex-col justify-center space-y-6 py-6 sm:w-[350px]">
                 <Link :href="route('home')" class="flex items-center text-lg font-medium w-fit mx-auto">
                     <AppLogoIcon class="block lg:hidden size-8 fill-current text-black dark:text-white" />
                 </Link>
